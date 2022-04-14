1b7af574c34759a72cca9435222662185bc64490
Marcos Valadão <marcosflobovaladao@gmail.com>
2022-04-14 17:27:15 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n=== TEST 3: seller findAllt ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
